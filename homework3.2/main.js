556c4135a7880cc08cdeeabb4ab8dbcae08e6144
Volodymyr Minin <vova.minin2010@gmail.com>
2024-05-27 09:49:25 +0300

    homework3.2 done

---
+++ homework3.2/main.js
@@ -0,0 +1,5 @@
+var first = prompt("Введіть перший рядок:");
+var second = prompt("Введіть другий рядок:");
+var third = prompt("Введіть третій рядок:");
+
+console.log(`${first}\n${second}\n${third}`);
