f2ca9774a531130ed44b98f2008a2cd40ee16685
Volodymyr Minin <vova.minin2010@gmail.com>
2024-08-28 16:30:31 +0300

    Homework may be done

---
+++ my-react-app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import TodoList from './components/TodoList';
+import './styles/styles.css';
+
+const App = () => (
+  <div>
+    <h1>Мій TODO список</h1>
+    <TodoList />
+  </div>
+);
+
+export default App;
